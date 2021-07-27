a80da988b25cbea32dafee723660d4acaaa84b5e
maryamraeisi <88035764+maryamraeisi@users.noreply.github.com>
2021-07-28 01:48:51 +0430

    Update practice-file.java

--- practice-file.java
+++ practice-file.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
+		//outputs Hello World!
 		System.out.println("Hello World!");
 		System.out.println("I'm trying to learn github.");
 		System.out.println("It's kind of fun!:)");
