9c692ddb0ecfd804f2474d3a7f073da747729c8f
maryamraeisi <88035764+maryamraeisi@users.noreply.github.com>
2021-07-27 21:55:17 +0430

    I've added the 3rd line.

--- practice-file.java
+++ practice-file.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("I'm trying to learn github.");
+		System.out.println("It's kind of fun!:)");
 	}
 }
